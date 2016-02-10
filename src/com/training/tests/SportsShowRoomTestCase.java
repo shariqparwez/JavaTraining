ee5872d7334f7f9fbaf525c525819139f5cc177e
Shariq Parwez <sparwe@sapient.com>
2016-02-10 15:31:42 +0530

    Feb 10th

---
+++ src/com/training/tests/SportsShowRoomTestCase.java
@@ -0,0 +1,31 @@
+package com.training.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.training.domains.*;
+import com.training.ifaces.Automobile;
+
+public class SportsShowRoomTestCase {
+
+	SportsShowRoom obj1 = null;
+	Automobile auto1 = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		obj1 = new SportsShowRoom();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCase1() {
+		//fail("Not yet implemented");
+		
+	}
+
+}
